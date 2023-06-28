eb41224b595d22a6901d39e3b8d770a287978acc
leonardovalde <leonardovalde16@gmail.com>
2023-06-28 14:04:20 -0500

    add crud methods

--- src/main/java/com/api/parkingcontrol/services/ParkingSpotService.java
+++ src/main/java/com/api/parkingcontrol/services/ParkingSpotService.java
@@ -1,5 +1,10 @@
 package com.api.parkingcontrol.services;
 
+import java.util.Optional;
+import java.util.UUID;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import com.api.parkingcontrol.models.ParkingSpotModel;
@@ -19,5 +24,24 @@ public class ParkingSpotService {
     @Transactional
     public ParkingSpotModel save(ParkingSpotModel parkingSpotModel) {
         return parkingSpotRepository.save(parkingSpotModel);
+    }
+    public boolean existsByLicensePlateCar(String licensePlateCar) {
+        return parkingSpotRepository.existsByLicensePlateCar(licensePlateCar);
+    }
+    public boolean existsByParkingSpotNumber(String parkingSpotNumber) {
+        return parkingSpotRepository.existsByParkingSpotNumber(parkingSpotNumber);
+    }
+    public boolean existsByApartmentAndBlock(String apartment, String apartmentBlock) {
+        return parkingSpotRepository.existsByApartmentAndApartmentBlock(apartment, apartmentBlock);
+    }
+    public Page<ParkingSpotModel> getAll(Pageable pageable) {
+        return parkingSpotRepository.findAll(pageable);
+    }
+    public Optional<ParkingSpotModel> getById(UUID id) {
+        return parkingSpotRepository.findById(id);
+    }
+    @Transactional
+    public void delete(UUID id) {
+        parkingSpotRepository.deleteById(id);
     }
 }
