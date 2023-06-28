128e5547fff1da220eae33c646c3fc16568172b4
leonardovalde <leonardovalde16@gmail.com>
2023-06-27 22:03:02 -0500

    initial commit

---
+++ src/main/java/com/api/parkingcontrol/dtos/ParkingSpotDto.java
@@ -0,0 +1,89 @@
+package com.api.parkingcontrol.dtos;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+public class ParkingSpotDto {
+
+    @NotBlank
+    private String parkingSpotNumber;
+    @NotBlank
+    @Size(max = 7)
+    private String licensePlateCar;
+    @NotBlank
+    private String brandCar;
+    @NotBlank
+    private String modelCar;
+    @NotBlank
+    private String colorCar;
+    @NotBlank
+    private String ownerName;
+    @NotBlank
+    private String apartment;
+    @NotBlank
+    private String apartmentBlock;
+    // Getters y Setters
+    public String getParkingSpotNumber() {
+        return parkingSpotNumber;
+    }
+
+    public void setParkingSpotNumber(String parkingSpotNumber) {
+        this.parkingSpotNumber = parkingSpotNumber;
+    }
+
+    public String getLicensePlateCar() {
+        return licensePlateCar;
+    }
+
+    public void setLicensePlateCar(String licensePlateCar) {
+        this.licensePlateCar = licensePlateCar;
+    }
+
+    public String getBrandCar() {
+        return brandCar;
+    }
+
+    public void setBrandCar(String brandCar) {
+        this.brandCar = brandCar;
+    }
+
+    public String getModelCar() {
+        return modelCar;
+    }
+
+    public void setModelCar(String modelCar) {
+        this.modelCar = modelCar;
+    }
+
+    public String getColorCar() {
+        return colorCar;
+    }
+
+    public void setColorCar(String colorCar) {
+        this.colorCar = colorCar;
+    }
+
+    public String getOwnerName() {
+        return ownerName;
+    }
+
+    public void setOwnerName(String ownerName) {
+        this.ownerName = ownerName;
+    }
+
+    public String getApartment() {
+        return apartment;
+    }
+
+    public void setApartment(String apartment) {
+        this.apartment = apartment;
+    }
+
+    public String getApartmentBlock() {
+        return apartmentBlock;
+    }
+
+    public void setApartmentBlock(String apartmentBlock) {
+        this.apartmentBlock = apartmentBlock;
+    }
+}
